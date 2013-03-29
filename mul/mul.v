05d94d2e8c2f0bcfe80e5d225d76670426d6f298
Xhacker Liu <liu.dongyuan@gmail.com>
2013-03-29 10:37:22 +0800

    multiplier.

---
+++ mul/mul.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+
+module mul(
+	input [7:0] multiplier,
+	input [7:0] multiplicand,
+	input clock,
+	output reg [7:0] result
+    );
+
+	reg [3:0] repetition;
+	reg [7:0] multiplier_reg, multiplicand_reg;
+	
+	initial begin
+		repetition = 0;
+	end
+	
+	always @(posedge clock) begin
+		if (repetition >= 4) repetition = 0;
+		if (repetition == 0) begin
+			multiplier_reg = multiplier;
+			multiplicand_reg = multiplicand;
+			result = 8'h00;
+		end
+		if (multiplier_reg[0]) result = result + multiplicand_reg;
+		multiplicand_reg = multiplicand_reg << 1;
+		multiplier_reg = multiplier_reg >> 1;
+		repetition = repetition + 1;
+	end
+
+endmodule
---
+++ mul/top.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+module top(
+	input clock,
+	input [1:0] switch,
+	input btn_increase,
+	input btn_clock,
+	output [7:0] segment,
+	output [3:0] anode
+    );
+
+	wire btn_increase_out, btn_clock_out;
+	debounce d1(clock, btn_increase, btn_increase_out);
+	debounce d2(clock, btn_clock, btn_clock_out);
+	
+	reg [3:0] multiplier, multiplicand;
+	initial begin
+		multiplier <= 4'h00;
+		multiplicand <= 4'h00;
+	end
+	
+	always @(posedge btn_increase_out) begin
+		if (switch[1]) multiplier <= multiplier + 1;
+		if (switch[0]) multiplicand <= multiplicand + 1;
+	end
+	
+	wire [7:0] result;
+	mul m1(multiplier, multiplicand, btn_clock_out, result);
+	
+	wire [15:0] disp_num;
+	assign disp_num = {multiplier, multiplicand, result};
+	display disp1(clock, disp_num, segment, anode);
+
+endmodule
