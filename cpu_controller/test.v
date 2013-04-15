aa8ebf05f0ec71577e0ff8718a45e4ec28a1d4d5
Xhacker Liu <liu.dongyuan@gmail.com>
2013-04-15 11:03:33 +0800

    CPU controller.

---
+++ cpu_controller/test.v
@@ -0,0 +1,55 @@
+module test;
+
+	// Inputs
+	reg [5:0] OP;
+
+	// Outputs
+	wire RegDst;
+	wire ALUsrcB;
+	wire MemToReg;
+	wire WriteReg;
+	wire MemWrite;
+	wire Branch;
+	wire ALUop1;
+	wire ALUop0;
+	wire JMP;
+
+	// Instantiate the Unit Under Test (UUT)
+	top uut (
+		.OP(OP), 
+		.RegDst(RegDst), 
+		.ALUsrcB(ALUsrcB), 
+		.MemToReg(MemToReg), 
+		.WriteReg(WriteReg), 
+		.MemWrite(MemWrite), 
+		.Branch(Branch), 
+		.ALUop1(ALUop1), 
+		.ALUop0(ALUop0), 
+		.JMP(JMP)
+	);
+
+	initial begin
+		// Initialize Inputs
+		OP = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		
+		OP = 6'b000000; // R
+		#200
+		OP = 6'b100011; // LW
+		#200
+		OP = 6'b101011; // SW
+		#200
+		OP = 6'b000100; // BEQ
+		#200
+		OP = 6'b000010; // JMP
+		#200
+		$stop;
+
+		// Add stimulus here
+
+	end
+      
+endmodule
+
